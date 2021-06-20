a7c3160a093de9ea85ad0cdb408eebb1259e60be
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-06-20 10:43:42 +0200

    Namespace library_travel

--- library_travel.js
+++ library_travel.js
@@ -1,13 +1,15 @@
-function computedTitle(e) {
-  const segments = e.field("Segments");
-  const head = ArrayExt.getHead(segments);
-  const last = ArrayExt.getLast(segments);
+const LibraryTravel = {
+  computedTitle: function (e) {
+    const segments = e.field("Segments");
+    const head = ArrayExt.getHead(segments);
+    const last = ArrayExt.getLast(segments);
 
-  const prefix = e.field("Type");
-  const suffix = ArrayExt.nonNull([
-    head ? head.field("computed_start") : null,
-    last ? last.field("computed_end") : null,
-  ]).join(" → ");
+    const prefix = e.field("Type");
+    const suffix = ArrayExt.nonNull([
+      head ? head.field("computed_start") : null,
+      last ? last.field("computed_end") : null,
+    ]).join(" → ");
 
-  return [prefix, suffix].join(" | ");
-}
+    return [prefix, suffix].join(" | ");
+  },
+};
