ea72191d96d341c89c75351bd67c451f38d44a8e
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-06-20 12:03:02 +0200

    Update library_entities

--- library_entities.js
+++ library_entities.js
@@ -1,12 +1,37 @@
 const LibraryEntities = {
   autofillImage: function () {
     switch (entry().field("type")) {
+      case "Board Game":
+        entry().set("image", entry().field("Board Games")[0].field("Image"));
+        break;
+      case "Book":
+        entry().set("image", entry().field("Books")[0].field("Cover"));
+        break;
       case "Coffee":
-        entry().set("image", entry().field("coffee")[0].field("image"));
+        entry().set("image", entry().field("Coffee")[0].field("image"));
         break;
       case "Game":
-        entry().set("image", entry().field("games")[0].field("Cover"));
+        entry().set("image", entry().field("Games")[0].field("Cover"));
         break;
+      case "Movie":
+        entry().set("image", entry().field("Movies")[0].field("Poster"));
+        break;
+    }
+  },
+  computedTitle: function () {
+    switch (field("type")) {
+      case "Board Game":
+        return field("movies")[0].field("Title");
+      case "Book":
+        return field("movies")[0].field("Title");
+      case "Coffee":
+        return field("coffee")[0].field("Title");
+      case "Game":
+        return field("games")[0].field("Title");
+      case "Location":
+        return field("locations")[0].field("Title");
+      case "Movie":
+        return field("movies")[0].field("Title");
     }
   },
   autofillAll: function () {
