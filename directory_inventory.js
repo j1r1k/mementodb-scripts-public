13fea6f643bbaa95ab8ba86853baff154f9f465a
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-06-20 15:58:59 +0200

    Add directory_inventory

---
+++ directory_inventory.js
@@ -0,0 +1,22 @@
+var DirectoryInventory = {
+  onCreatePost: function () {
+    var created = entry();
+
+    var newEntity = libByName("entities").create({});
+
+    newEntity.set("type", "Item");
+    newEntity.set("inventory", [created]);
+    newEntity.set("image", created.field("image"));
+    newEntity.set("computed_title", created.field("Title"));
+
+    var newEvent = libByName("events").create({});
+    newEvent.set("type", "Inventory");
+    newEvent.set("title", "Added to inventory");
+    newEvent.set("start date", created.field("added"));
+    newEvent.set(
+      "computed_start",
+      moment(created.field("added")).startOf("day")
+    );
+    newEntity.set("inventory", [created]);
+  },
+};
