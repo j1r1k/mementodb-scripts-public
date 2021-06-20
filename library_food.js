c1cd04c774eadab1147157357c83cc23866395fd
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-06-20 11:41:09 +0200

    Cleanup library_food

--- library_food.js
+++ library_food.js
@@ -1,26 +1,43 @@
-function computedProperty(key) {
-  return field("Products")
-    .map(function (item) {
-      var weight = 0;
-      if (item.attr("Weight") > 0) {
-        weight = item.attr("Weight");
-      } else {
-        weight =
-          item.attr("Quantity") *
-          item.field("computed_" + item.attr("serving"));
-      }
-      return (item.field("computed_" + key) / 100.0) * weight;
-    })
-    .reduce(function (a, b) {
-      return a + b;
-    }, 0)
-    .toFixed(2);
-}
+const LibraryFood = {
+  computedProperty: function (key) {
+    return field("Products")
+      .map(function (item) {
+        var weight = 0;
+        if (item.attr("Weight") > 0) {
+          weight = item.attr("Weight");
+        } else {
+          weight =
+            item.attr("Quantity") *
+            item.field("computed_" + item.attr("serving"));
+        }
+        return (item.field("computed_" + key) / 100.0) * weight;
+      })
+      .reduce(function (a, b) {
+        return a + b;
+      }, 0)
+      .toFixed(2);
+  },
 
-function computedTitle(delimiter) {
-  return field("Products")
-    .map(function (item) {
-      item.field("Title");
-    })
-    .join(delimiter);
-}
+  computedDescription: function () {
+    return [
+      "pr:",
+      field("computed_protein"),
+      "| f:",
+      field("computed_fat"),
+      "| ch:",
+      field("computed_carbohydrate"),
+    ].join(" ");
+  },
+
+  computedCaloriesLabel: function () {
+    return field("computed_calories") + "cal";
+  },
+
+  computedTitle: function () {
+    return field("Products")
+      .map(function (item) {
+        item.field("Title");
+      })
+      .join(" | ");
+  },
+};
