3cf5c3ffa34f10947a00b47e1d8d5474ef6aae3e
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-06-20 10:45:54 +0200

    Fix library travel

--- library_travel.js
+++ library_travel.js
@@ -1,10 +1,10 @@
 const LibraryTravel = {
-  computedTitle: function (e) {
-    const segments = e.field("Segments");
+  computedTitle: function (getField) {
+    const segments = getField("Segments");
     const head = ArrayExt.getHead(segments);
     const last = ArrayExt.getLast(segments);
 
-    const prefix = e.field("Type");
+    const prefix = getField("Type");
     const suffix = ArrayExt.nonNull([
       head ? head.field("computed_start") : null,
       last ? last.field("computed_end") : null,
