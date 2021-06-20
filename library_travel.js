40b06fba0c432dc4a590bea6395b6548e42b9af8
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-06-20 10:47:46 +0200

    Get rid of getField

--- library_travel.js
+++ library_travel.js
@@ -1,15 +1,12 @@
 const LibraryTravel = {
-  computedTitle: function (getField) {
-    const segments = getField("Segments");
+  computedTitle: function (type, segments) {
     const head = ArrayExt.getHead(segments);
     const last = ArrayExt.getLast(segments);
-
-    const prefix = getField("Type");
     const suffix = ArrayExt.nonNull([
       head ? head.field("computed_start") : null,
       last ? last.field("computed_end") : null,
     ]).join(" → ");
 
-    return [prefix, suffix].join(" | ");
+    return [type, suffix].join(" | ");
   },
 };
