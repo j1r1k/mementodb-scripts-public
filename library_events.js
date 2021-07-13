f0e3dd03ea9accc91ed6a5db5116cee4a695a484
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-07-13 20:14:43 +0200

    Add LibraryEvents computedStatus

--- library_events.js
+++ library_events.js
@@ -1,4 +1,18 @@
 var LibraryEvents = {
+  computedStatus: function () {
+    const durationHours = field("autofill_duration") / 3600000;
+
+    if (durationHours >= 24) {
+      return field("computed_days") + " days";
+    } else {
+      const duration = moment.duration(field("autofill_duration"));
+      return [
+        String(duration.hours()).padStart(2, "0"),
+        String(duration.minutes()).padStart(2, "0"),
+        String(duration.seconds()).padStart(2, "0"),
+      ].join(":");
+    }
+  },
   autofillAll: function () {
     LibraryCommon.autofillTime(entry());
   },
