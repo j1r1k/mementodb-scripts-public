761bb2a2947b3040e7bb81a212172312f135854a
Jiri Marsicek <jiri.marsicek@gmail.com>
2021-08-12 21:43:44 +0200

    Manual travel record

--- library_travel.js
+++ library_travel.js
@@ -11,30 +11,43 @@ var LibraryTravel = {
     ].join(" ");
   },
   computedTitle: function () {
-    const type = field("Type");
-    const routes = field("Routes");
-    const head = ArrayExt.getHead(routes);
-    const last = ArrayExt.getLast(routes);
-    const suffix = ArrayExt.nonNull([
-      head ? head.field("computed_start") : null,
-      last ? last.field("computed_end") : null,
-    ]).join(" → ");
+    const type = field("type");
+    const activity = field("Activity");
+    if (type === "Manual") {
+      return [activity, field("Title")].join(" | ")
+    } else {
+      const routes = field("Routes");
+      const head = ArrayExt.getHead(routes);
+      const last = ArrayExt.getLast(routes);
+      const suffix = ArrayExt.nonNull([
+        head ? head.field("computed_start") : null,
+        last ? last.field("computed_end") : null,
+      ]).join(" → ");
 
-    return [type, suffix].join(" | ");
+      return [activity, suffix].join(" | ");
+    }
   },
   autofillAll: function () {
     LibraryCommon.autofillTime(entry());
 
-    const routes = entry().field("Routes");
-    entry().set("autofill_length", EntriesExt.sumRelated(routes, "length"));
-    entry().set(
-      "autofill_elevation_up",
-      EntriesExt.sumRelated(routes, "Elevation Up")
-    );
-    entry().set(
-      "autofill_elevation_down",
-      EntriesExt.sumRelated(routes, "Elevation Down")
-    );
+    const type = field("Type");
+    
+    if (type === "Manual") {
+      entry().set("autofill_length", field("Length"));
+      entry().set("autofill_elevation_up", field("Elevation Up"));
+      entry().set("autofill_elevation_down", field("Elevation Down"));
+    } else {
+      const routes = entry().field("Routes");
+      entry().set("autofill_length", EntriesExt.sumRelated(routes, "length"));
+      entry().set(
+        "autofill_elevation_up",
+        EntriesExt.sumRelated(routes, "Elevation Up")
+      );
+      entry().set(
+        "autofill_elevation_down",
+        EntriesExt.sumRelated(routes, "Elevation Down")
+      );
+    }
   },
   onUpdatePost: function () {
     LibraryTravel.autofillAll();
